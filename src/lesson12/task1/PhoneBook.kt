e232f6872b74143e323837e568064cbc94eef31b
Mexus209 <spikemorow@gmail.com>
2022-03-07 16:47:43 +0300

    lesson 12 task PhoneBook

--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -2,6 +2,7 @@
 
 package lesson12.task1
 
+
 /**
  * Класс "Телефонная книга".
  *
@@ -18,13 +19,21 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    private var map = mutableMapOf<String, List<String>?>()
+
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean {
+        if (name in map.keys) return false
+        else {
+            map[name] = null
+        }
+        return true
+    }
 
     /**
      * Убрать человека.
@@ -32,7 +41,13 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean {
+        if (name !in map.keys) return false
+        else {
+            map.remove(name)
+        }
+        return true
+    }
 
     /**
      * Добавить номер телефона.
@@ -41,7 +56,17 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean {
+        for ((_, value) in map) {
+            if (value?.contains(phone) == true) return false
+        }
+        if (name !in map.keys) return false
+        else {
+            if (map[name] == null) map[name] = listOf(phone)
+            if (map[name] != null) map[name] = map[name]?.plus(listOf(phone))
+        }
+        return true
+    }
 
     /**
      * Убрать номер телефона.
@@ -49,24 +74,58 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean {
+        for ((key, value) in map) {
+            if (key == name && value?.contains(phone) == false) return false
+        }
+        if (name !in map.keys) return false
+        else {
+            map[name] = map[name]?.filter { it != phone }
+        }
+        return true
+    }
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> {
+        return if (name !in map.keys || map[name] == null) setOf()
+        else map[name]!!.toSet()
+    }
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? {
+        for ((key, value) in map) {
+            if (value?.contains(phone) == true) return key
+        }
+        return null
+    }
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        val book = this.map
+        var book2 = mutableMapOf<String, List<String>?>()
+        if (other is PhoneBook) book2 = other.map
+        else return false
+        val list = mutableListOf<String>()
+        for ((key, _) in book2) list.add(key)
+        val result = book - list
+        return result.isEmpty() && book.size == book2.size
+    }
+
+    override fun hashCode(): Int {
+        var hash = 21
+        hash = 31 * hash + map.size
+        hash += map.forEach { it.value }.hashCode()
+        hash += map.forEach { it.key }.hashCode()
+        return hash
+    }
 }
